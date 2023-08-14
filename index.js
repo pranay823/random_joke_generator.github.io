1dfe18085dceadf063f7822635d1e7a34d8491ae
pranay823 <102896608+pranay823@users.noreply.github.com>
2023-08-14 14:52:18 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const joke = document.querySelector(".joke-container");
+const jokeBtn = document.querySelector(".joke-generator");
+
+window.addEventListener("DOMContentLoaded",function(){
+    getjoke();
+})
+
+let url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&idRange=0-200";
+
+ async function getjoke(){
+    joke.classList.remove("fade");
+    let content =  await fetch(url);
+    let response = await content.json();
+    joke.textContent = `${response.joke}`
+    joke.classList.add("fade");
+}
+
+jokeBtn.addEventListener("click" , getjoke)
